2e9b7be1a478a695ff0cf3497de4582ffcc23e1f
Hannah L <hannahlivnat@gmail.com>
2020-05-14 12:37:54 -0700

    fix image links

---
+++ portfolio-website/app.js
@@ -0,0 +1,6 @@
+//loop to apply alternative styling to even card background colors - 
+//every even card sea-blue;
+
+//project lis are attached to specific divs or sections
+//when they are clicked on, the event listener triggers
+//that div to appear.
